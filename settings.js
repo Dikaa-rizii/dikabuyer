800e9968ee391951899f7795bb50314631d63757
Dikaa-rizii <112540130+Dikaa-rizii@users.noreply.github.com>
2022-10-02 20:30:31 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,8 +26,8 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Haikyu BOTZ" //namabot kalian
-global.ownername= "HaikyuElite" //nama kalian
+global.botname = "Candra BOTZ" //namabot kalian
+global.ownername= "CandraElite" //nama kalian
 global.myweb ="https://api.haikyuofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/Haikyu" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
@@ -35,15 +35,15 @@ global.email = "haikyuofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['62887435047326'] //premium user
-global.owner = ["62895403431445","6285220625571"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62895403431445" // nomor wa kalian
-global.ownernomerr = "+62895403431445" //nmr wa kalian
+global.owner = ["6287785833132","6285220625571"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6287785833132" // nomor wa kalian
+global.ownernomerr = "+6287785833132" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = 'HaikyuBotz' //sticker wm ubah
-global.author = 'Di Buat Oleh HaikyuWangsaft' //sticker wm ganti nama kalian
+global.packname = 'CandraBotz' //sticker wm ubah
+global.author = 'Di Buat Oleh CandraWangsaft' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
